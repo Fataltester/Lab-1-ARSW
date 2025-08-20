85d5d971a74e2e9b091781760d75bedd98a0c1ef
Fataltester <juanm46891@gmail.com>
2025-08-20 14:11:26 -0500

    primer punto realizado, falta justificacion

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.threads;
-
+import java.util.Timer;
+import java.util.TimerTask;
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread{
+    private int A = 0;
+    private int B = 0;
+    public  void counting(int a,int b){
+        CountThread thread = new CountThread();
+        A = a;
+        B = b;
+    }
+
+    public void run(){
+        Timer timer = new Timer();
+        long tiempoInicio = System.currentTimeMillis();
+        System.out.println("Tiempo inicial(ms): "+ tiempoInicio);
+        long tiempoTranscurrido = System.currentTimeMillis() - tiempoInicio;
+        while(A <= B){
+            System.out.println(A);
+             A += 1;
+        }
+        System.out.println("Terminado..");
+        System.out.println("Tiempo Final(ms): "+ tiempoTranscurrido);
+        timer.cancel();
+    }
+        
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.threads;
-
+import edu.eci.arsw.threads.CountThread;
 /**
  *
  * @author hcadavid
@@ -12,7 +12,17 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
+        CountThread thread1 = new CountThread();
+        CountThread thread2 = new CountThread();
+        CountThread thread3 = new CountThread();
         
+        thread1.counting(0,99);
+        thread2.counting(99,199);
+        thread3.counting(200,299);
+        
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
     
 }
